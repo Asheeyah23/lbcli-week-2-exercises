f70723267c01f5e26d0de0ee8e0384b7f56f5103
Asheeyah23 <ajokeashiah7@gmail.com>
2026-07-12 11:34:23 +0100

    Store new address and generate blocks to it

--- submission/03.sh
+++ submission/03.sh
@@ -1,1 +1,3 @@
-bitcoin-cli -regtest -rpcwallet="btrustwallet" getnewaddress "" bech32
+addr=$(bitcoin-cli -regtest -rpcwallet="btrustwallet" getnewaddress "" bech32)
+bitcoin-cli -regtest generatetoaddress 101 "$addr" > /dev/null
+echo "$addr"
